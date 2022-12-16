17bbd2917eb9a008ad4ee1e86fd2cc60737ccd56
dashapr <dashaprud@rambler.ru>
2022-12-16 12:50:26 +0300

    Домашнее задание 7

--- script.js
+++ script.js
@@ -1,86 +1,200 @@
-const items = [
-  {
-    title: "Игрушка мячик",
-    description: "Ваш питомец будет счастлив!",
-    tags: ["cat", "dog"],
-    price: 500,
-    img: "./img/1.jpeg",
-  },
-  {
-    title: "Игрушка лабиринт",
-    description: "Поможет в развитии интеллекта!",
-    tags: ["cat", "dog"],
-    price: 900,
-    img: "./img/2.jpeg",
-  },
-  {
-    title: "Игрушка для котят",
-    description: "Отвлечет вашего питомца!",
-    tags: ["cat"],
-    price: 300,
-    img: "./img/3.jpeg",
-  },
-  {
-    title: "Миска «Котик»",
-    description: "Подойдет и для собак!",
-    tags: ["cat", "dog"],
-    price: 660,
-    img: "./img/4.jpeg",
-  },
-  {
-    title: "Лоток розовый",
-    description: "Теперь вы можете забыть о проблемах с туалетом",
-    tags: ["cat"],
-    price: 400,
-    img: "./img/5.jpeg",
-  },
-  {
-    title: "Сухой корм для кошек",
-    description: "Специальная формула для милых усатиков!",
-    tags: ["cat"],
-    price: 200,
-    img: "./img/6.jpeg",
-  },
-  {
-    title: "Сухой корм для собак",
-    description: "Содержит полный комплекс витаминов",
-    tags: ["dog"],
-    price: 300,
-    img: "./img/7.jpeg",
-  },
-  {
-    title: "Игрушка для собак",
-    description: "Теперь вы можете не переживать за личные вещи",
-    tags: ["dog"],
-    price: 500,
-    img: "./img/8.jpeg",
-  },
-  {
-    title: "Лежанка",
-    description: "Идеальное место для отдыха!",
-    tags: ["cat", "dog"],
-    price: 1500,
-    img: "./img/9.jpeg",
-  },
-  {
-    title: "Поилка для собак",
-    description: "Возьмите с собой в путешествие",
-    tags: ["dog"],
-    price: 800,
-    img: "./img/10.jpeg",
-  },
-  {
-    title: "Переноска",
-    description: "Путешествуйте с комфортом!",
-    tags: ["cat", "dog"],
-    price: 3500,
-    img: "./img/11.jpeg",
-  },
-  {
-    title: "Поводок для собак",
-    description: "Для чудесных прогулок вместе",
-    tags: ["dog"],
-    price: 800,
-    img: "./img/12.jpeg",
-  },
+const items = [{
+        title: "Игрушка мячик",
+        description: "Ваш питомец будет счастлив!",
+        tags: ["cat", "dog"],
+        price: 500,
+        img: "./img/1.jpeg",
+    },
+    {
+        title: "Игрушка лабиринт",
+        description: "Поможет в развитии интеллекта!",
+        tags: ["cat", "dog"],
+        price: 900,
+        img: "./img/2.jpeg",
+    },
+    {
+        title: "Игрушка для котят",
+        description: "Отвлечет вашего питомца!",
+        tags: ["cat"],
+        price: 300,
+        img: "./img/3.jpeg",
+    },
+    {
+        title: "Миска «Котик»",
+        description: "Подойдет и для собак!",
+        tags: ["cat", "dog"],
+        price: 660,
+        img: "./img/4.jpeg",
+    },
+    {
+        title: "Лоток розовый",
+        description: "Теперь вы можете забыть о проблемах с туалетом",
+        tags: ["cat"],
+        price: 400,
+        img: "./img/5.jpeg",
+    },
+    {
+        title: "Сухой корм для кошек",
+        description: "Специальная формула для милых усатиков!",
+        tags: ["cat"],
+        price: 200,
+        img: "./img/6.jpeg",
+    },
+    {
+        title: "Сухой корм для собак",
+        description: "Содержит полный комплекс витаминов",
+        tags: ["dog"],
+        price: 300,
+        img: "./img/7.jpeg",
+    },
+    {
+        title: "Игрушка для собак",
+        description: "Теперь вы можете не переживать за личные вещи",
+        tags: ["dog"],
+        price: 500,
+        img: "./img/8.jpeg",
+    },
+    {
+        title: "Лежанка",
+        description: "Идеальное место для отдыха!",
+        tags: ["cat", "dog"],
+        price: 1500,
+        img: "./img/9.jpeg",
+    },
+    {
+        title: "Поилка для собак",
+        description: "Возьмите с собой в путешествие",
+        tags: ["dog"],
+        price: 800,
+        img: "./img/10.jpeg",
+    },
+    {
+        title: "Переноска",
+        description: "Путешествуйте с комфортом!",
+        tags: ["cat", "dog"],
+        price: 3500,
+        img: "./img/11.jpeg",
+    },
+    {
+        title: "Поводок для собак",
+        description: "Для чудесных прогулок вместе",
+        tags: ["dog"],
+        price: 800,
+        img: "./img/12.jpeg",
+    },
 ];
+
+
+
+const itemTemplate = document.querySelector('#item-template');
+const itemsContainer = document.querySelector('#shop-items');
+
+
+function prepareShopItem(shopItem) {
+    // Деструктурируем свойства объекта
+    const { title, description, tags, img, price } = shopItem;
+
+    const item = itemTemplate.content.cloneNode(true);
+
+    item.querySelector("h1").textContent = title;
+    item.querySelector("p").textContent = description;
+    item.querySelector("img").src = img;
+    item.querySelector(".price").textContent = `${price}P`;
+
+
+
+    // Находим шаблон для тегов
+    const tagsHolder = item.querySelector(".tags");
+
+    // Отрисовываем теги для товара
+    tags.forEach((tag) => {
+        const element = document.createElement("span");
+        element.textContent = tag;
+        element.classList.add("tag");
+        tagsHolder.append(element);
+    });
+
+    return item;
+}
+
+
+items.forEach((item) => {
+    itemsContainer.append(prepareShopItem(item));
+});
+
+// Инпут для поиска
+const searchInput = document.querySelector("#search-input");
+// Кнопка
+const searchButton = document.querySelector("#search-btn");
+
+// Функция для поиска по товарам (сбрасывает фильтры)
+function applySearch() {
+    // Взяли значение инпута и "причесали" его
+    // Привели к нижнему регистру, чтобы написание не мешало поиску
+    const searchString = searchInput.value.trim().toLowerCase();
+
+    // Нашли все товары, в title которых есть searchString
+    currentState = items.filter((el) =>
+        el.title.toLowerCase().includes(searchString)
+    );
+    // Отсортировали их по алфавиту
+    currentState.sort((a, b) => sortByAlphabet(a, b));
+    // Отрисовали результаты поиска
+    renderItems(currentState);
+    // По умолчанию сортировка "по алфавиту"
+    sortControl.selectedIndex = 0;
+}
+
+// Обработчик при клике на кнопку поиска
+searchButton.addEventListener("click", applySearch);
+// Обработчик события поиска при взаимодействии с инпутом
+searchInput.addEventListener("search", applySearch);
+
+
+
+
+
+// Товары после применения поиска / фильтров
+// которые мы будем показывать пользователю
+let currentState = [...items];
+
+// Текст, если ничего не найдено
+const nothingFound = document.querySelector("#nothing-found");
+
+// Функция для отрисовки
+// В качестве параметра — товары, которые нужно отрисовать
+function renderItems(arr) {
+    // Сбрасываем текст "Ничего не найдено" после предыдущего поиска
+    nothingFound.textContent = "";
+    // И чистим контейнер с товарами на случай, если там что-то было
+    itemsContainer.innerHTML = "";
+    // Отрисовываем товары из переданного параметра arr
+    arr.forEach((item) => {
+        // Вызываем prepareShopItem для каждого товара
+        // И подставляем результат в верстку
+        itemsContainer.append(prepareShopItem(item));
+    });
+    // Если массив товаров пустой, отображаем текст, что ничего не нашлось
+    if (!arr.length) {
+        nothingFound.textContent = "Ничего не найдено";
+    }
+}
+
+// Функция-хелпер для сортировки товаров по алфавиту
+function sortByAlphabet(a, b) {
+    // Смотрим на свойство title
+    // Если title первого товара алфавитно больше второго...
+    if (a.title > b.title) {
+        return 1;
+    }
+    // Если title второго товара больше
+    if (a.title < b.title) {
+        return -1;
+    }
+    // Если они равны
+    return 0;
+}
+
+// Вызываем функцию для отрисовки в самом начале
+// И тут же сортируем по алфавиту
+renderItems(currentState.sort((a, b) => sortByAlphabet(a, b)));
